dcaef3a0861ae1d6371988356793f5a34c2e9737
Liz Vanderkloot <liz@barbershoplabs.com>
2015-05-13 10:56:36 -0400

    Authenticated user may downvote a question

--- spec/features/vote_question_spec.rb
+++ spec/features/vote_question_spec.rb
@@ -12,7 +12,7 @@ feature "vote on a question", %{
   scenario "authenticated user upvotes question", js: true, driver: :webkit do
     sign_in user
     visit question_path(question)
-    
+
     within("#question-#{question.id}") do
       find(:css, '.upvote').trigger('click')
     end
@@ -21,7 +21,22 @@ feature "vote on a question", %{
     end
   end
 
-  pending "authenticated user downvotes question"
-  pending "user has voted removes vote"
+  pending "authenticated user downvotes question" , js: true, driver: :webkit do
+    sign_in user
+    visit question_path(question)
+
+    within("#question-#{question.id}") do
+      find(:css, '.downvote').trigger('click')
+    end
+    within("#question-#{question.id} .vote-total") do
+      expect(page).to have_content("-1")
+    end
+  end
+
+  pending "authenticated user removes upvote by clicking upvote again"
+  pending "authenticated user removes downvote by clicking downvote again"
+  pending "authenticated user removes downvote by clicking downvote again"
+  pending "authenticated user switches upvote to downvote"
+  pending "authenticated user switches downvote to upvote"
   pending "unauthenticated visitor may not vote"
 end
